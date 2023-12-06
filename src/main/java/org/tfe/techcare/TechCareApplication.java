a21756a1eef51d58effa0967849adc5a051e9d12
LeonardoGarciaMelo <lgarciamelo213@gmail.com>
2023-12-06 09:28:42 -0300

    first commit

---
+++ src/main/java/org/tfe/techcare/TechCareApplication.java
@@ -0,0 +1,13 @@
+package org.tfe.techcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechCareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/tfe/techcare/TechCareApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tfe.techcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
