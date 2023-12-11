ed4a109f79fa5fa3f7281bf172fedb98302ba206
Kaue Miziara <kauemiziara@gmail.com>
2023-12-11 13:40:49 -0300

    Migrate database: SQLite replaced by H2

--- src/main/resources/db/migration/V1__create-table-pacients.sql
+++ src/main/resources/db/migration/V1__create-table-pacients.sql
@@ -1,12 +1,12 @@
-CREATE TABLE pacients(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL,
+CREATE TABLE pacients (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
     age INTEGER NOT NULL,
-    gender TEXT NOT NULL,
-    phone TEXT NOT NULL,
-    email TEXT NOT NULL,
-    city TEXT NOT NULL,
-    state TEXT NOT NULL,
-    postal_code TEXT NOT NULL,
-    diagnosis TEXT NOT NULL
-)
+    gender VARCHAR(255) NOT NULL,
+    phone VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    city VARCHAR(255) NOT NULL,
+    state VARCHAR(255) NOT NULL,
+    postal_code VARCHAR(255) NOT NULL,
+    diagnosis VARCHAR(255) NOT NULL
+);
